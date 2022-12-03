5a9214a3e2127e9960b5f0ad3b15b437d6d96df7
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-03 23:38:22 +0500

    4z_last

--- 4z/Program.cs
+++ 4z/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
