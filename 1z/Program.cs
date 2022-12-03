6350790532714b6072dea769c5a9d9838b520967
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-03 23:16:25 +0500

    1z

---
+++ 1z/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (n1 > n2)
+{
+    max = n1;
+    Console.WriteLine($"max = {max}");
+}
+else
+{
+    max = n2;
+    Console.WriteLine($"max = {max}");
+}
