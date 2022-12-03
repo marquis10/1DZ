a8b84005c7fa29ac56b7aec3caf068d92492f0fb
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-03 23:18:31 +0500

    3z

---
+++ 3z/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+    {
+        Console.WriteLine($"{n} является чётным числом");
+    }
+else
+{
+    Console.WriteLine($"{n} не является чётным числом");
+}
